3f81a56ed27d4aa6d52285a8ac5da70bb0086969
Aamosh12 <123634110+Aamosh12@users.noreply.github.com>
2023-02-01 16:57:57 +0545

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ extensions = [
 #     'python': ('https://docs.python.org/3/', None),
 #     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 # }
-# intersphinx_disabled_domains = ['std']
+# intersphinx_disabled_domains = []
 
 templates_path = ['_templates']
 
