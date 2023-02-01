8c7a272c3a475031c7cda676410ffa5f323271cf
Aamosh12 <123634110+Aamosh12@users.noreply.github.com>
2023-02-01 16:22:19 +0545

    disabled domains deleted

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['_std']
+
 
 templates_path = ['_templates']
 
