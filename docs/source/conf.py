dcda3ce3070c04ea9219b574f2b74f8d2913e121
Aamosh12 <123634110+Aamosh12@users.noreply.github.com>
2023-02-01 16:09:39 +0545

    Theme-Restored

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ['sphinx-rtd-theme']
 
 templates_path = ['_templates']
 
